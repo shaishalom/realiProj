41d80bc2f7797852bbbaa299b20fc29d31c7a06d
shai shalom <shaishalom1@gmail.com>
2020-11-16 23:17:38 +0200

    remove comments in the file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,48 +1,3 @@
-DROP TABLE IF EXISTS quote;
- 
-CREATE TABLE quote (
-  id INTEGER   PRIMARY KEY auto_increment,
-  name VARCHAR(250) NOT NULL,
-  price DOUBLE NOT NULL
-);
- 
-DROP TABLE IF EXISTS item;
- 
-CREATE TABLE item (
-  id INTEGER   PRIMARY KEY auto_increment,
-  name VARCHAR(250) NOT NULL,
-  quote_id INTEGER, 
-  FOREIGN KEY (quote_id) REFERENCES quote(id)
-);
-
-
-INSERT INTO quote (id, name, price)
-VALUES
-  (1, 'bezeq', 100),
-  (2, 'teva', 200),
-  (3, 'delek', 300);  
-  
-  
-INSERT INTO item (id, name, quote_id)
-VALUES
-  (1, 'shai',1),
-  (2, 'shai',2),
-  (3, 'shai',3),
-  (4, 'golan',1),
-    (5, 'dani',2);
-  
-  
-DROP TABLE IF EXISTS quoteLog;
- 
-CREATE TABLE quoteLog (
-  id INTEGER   PRIMARY KEY auto_increment,
-  quote_id INTEGER   ,
-  created_date TIMESTAMP NOT NULL,
-  quote_operation VARCHAR(250) NOT NULL,
-  error_code INTEGER ,
-  message VARCHAR(250) NOT NULL,
-);
-
 DROP TABLE IF EXISTS users;
  
 CREATE TABLE  users(
@@ -83,11 +38,4 @@ VALUES
   (2, 'ROLE_MODERATOR'),
   (3, 'ROLE_ADMIN');
   
---  insert into user_roles(user_id,role_id) values
---  (1,1),
---  (1,2),
---  (1,3),
---  (2,1),
---  (2,2),
---  (2,3);
-  
+
